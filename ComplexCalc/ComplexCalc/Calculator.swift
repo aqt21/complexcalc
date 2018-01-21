bc43600e98e501e4b669d96c760f6b4cf505d4b0
Andrew Tran <andrewtran@Andrews-Mac.local>
2018-01-21 07:11:27 -0800

    Completed all functions except MathOP

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,114 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return (lhs + rhs)
+    }
+    
+    func add(_ nums: [Int]) -> Int {
+        var total : Int = 0
+        for i in 0 ... nums.count - 1 {
+            total += nums[i]
+        }
+        
+        return total
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        var total : (Int, Int) = (0, 0)
+        total.0 = lhs.0 + rhs.0
+        total.1 = lhs.1 + rhs.1
+        return total
+    }
+    
+    func add(lhs: [String : Int], rhs: [String : Int]) -> [String : Int] {
+        var total : [String : Int] = ["x": 0, "y": 0]
+        total["x"]! = rhs["x"]! + lhs["x"]!
+        total["y"]! = rhs["y"]! + lhs["y"]!
+        return total
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return (lhs - rhs)
+    }
+    
+    func subtract(_ nums: [Int]) -> Int {
+        var total : Int = 0
+        for i in 0 ... nums.count - 1 {
+            total -= nums[i]
+        }
+        
+        return total
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        var total : (Int, Int) = (0, 0)
+        total.0 = lhs.0 - rhs.0
+        total.1 = lhs.1 - rhs.1
+        return total
+    }
+    
+    func subtract(lhs: [String : Int], rhs: [String : Int]) -> [String : Int] {
+        var total : [String : Int] = ["x": 0, "y": 0]
+        total["x"]! = lhs["x"]! - rhs["x"]!
+        total["y"]! = lhs["y"]! - rhs["y"]!
+        return total
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return (lhs * rhs)
+    }
+    
+    func multiply(_ nums: [Int]) -> Int {
+        var total : Int = nums[0]
+        for i in 1 ... nums.count - 1 {
+            total *= nums[i]
+        }
+        
+        return total
+    }
+    
+    func multiply(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        var total : (Int, Int) = (0, 0)
+        total.0 = lhs.0 * rhs.0
+        total.1 = lhs.1 * rhs.1
+        return total
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return (lhs / rhs)
+    }
+    
+    func divide(_ nums: [Int]) -> Int {
+        var total : Int = nums[0]
+        for i in 1 ... nums.count - 1 {
+            total /= nums[i]
+        }
+        
+        return total
+    }
+    
+    func divide(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        var total : (Int, Int) = (0, 0)
+        total.0 = lhs.0 / rhs.0
+        total.1 = lhs.1 / rhs.1
+        return total
+    }
+    
+    func count(_ nums: [Int]) -> Int {
+        return nums.count
+    }
+    
+    func avg(_ nums: [Int]) -> Int {
+        var total : Int = 0
+        for i in 0 ... nums.count - 1 {
+            total += nums[i]
+        }
+        
+        return total / nums.count
+    }
+    
+    static func mathOp(lhs: Int, rhs: Int, op: String ) -> Int {
+        return (lhs + rhs)
+    }
 }
